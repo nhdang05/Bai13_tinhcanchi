2f9900ebc3d5ba8a6d30ad7f0bd029ebe3baeee8
MAY 06 <MAY 06@LAB2-05>
2025-02-24 11:25:04 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Bai13_tinhcanchi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNam = new System.Windows.Forms.TextBox();
+            this.txtCanChi = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lstNam = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Năm";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kết Quả";
+            // 
+            // txtNam
+            // 
+            this.txtNam.Location = new System.Drawing.Point(151, 60);
+            this.txtNam.Name = "txtNam";
+            this.txtNam.Size = new System.Drawing.Size(235, 20);
+            this.txtNam.TabIndex = 2;
+            // 
+            // txtCanChi
+            // 
+            this.txtCanChi.Location = new System.Drawing.Point(151, 116);
+            this.txtCanChi.Name = "txtCanChi";
+            this.txtCanChi.Size = new System.Drawing.Size(235, 20);
+            this.txtCanChi.TabIndex = 2;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(451, 60);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(55, 30);
+            this.btnTinh.TabIndex = 3;
+            this.btnTinh.Text = "button1";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(451, 116);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(56, 27);
+            this.btnThoat.TabIndex = 4;
+            this.btnThoat.Text = "button2";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // lstNam
+            // 
+            this.lstNam.FormattingEnabled = true;
+            this.lstNam.Location = new System.Drawing.Point(564, 50);
+            this.lstNam.Name = "lstNam";
+            this.lstNam.Size = new System.Drawing.Size(200, 290);
+            this.lstNam.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstNam);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.txtCanChi);
+            this.Controls.Add(this.txtNam);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNam;
+        private System.Windows.Forms.TextBox txtCanChi;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ListBox lstNam;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai13_tinhcanchi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string[] canArr = { "Canh", "Tân", "Nhâm", "Canh", "Tân", "Nhâm","Canh", "Tân", "Nhâm","Canh", "Tân", "Nhâm","Nhâm" };
+            string[] chiArr = { "thân", "dậu", "tuất", "Canh", "Tân", "Nhâm","Canh", "Tân", "Nhâm","Canh", "Tân", "Nhâm" };
+
+            string giatritrave = canArr[Convert.ToInt32(txtNam.Text) % 10] + " " + chiArr[Convert.ToInt32(txtNam.Text) % 12];
+            txtCanChi.Text = giatritrave;   
+        }
+    }
+}
